4ee31fa8e01bec4feb3ac52f5f7a7919c8d5c429
noisyyyy <129625403+noisyyyy@users.noreply.github.com>
2024-05-02 22:05:04 +0200

    Update offsets.hpp

--- offsets.hpp
+++ offsets.hpp
@@ -1,1 +1,48 @@
+uworld/gworld -> 0x12634238
+staticfindobject -> 0x13A8170
+getbonematrix -> 0x1C7FD2C
 
+
+// ud sources -> https://discord.gg/2y4ff5wyNX
+
+#include <cstdint>
+  
+namespace offsets
+{
+uintptr_t UWorld = 0x12634238;
+uintptr_t GNames = 0x127F3180;
+uintptr_t GameState = 0x160;
+uintptr_t OwningGameInstance = 0x1D8;
+uintptr_t LocalPlayers = 0x38;
+uintptr_t PlayerController -> 0x30;
+uintptr_t LocalPawn = 0x338;
+uintptr_t PlayerState = 0x2B0;
+uintptr_t RootComponent = 0x198;
+uintptr_t PersistentLevel = 0x30;
+uintptr_t ReviveFromDBNOTime = 0x4BC0;
+uintptr_t Mesh = 0x318;
+uintptr_t TeamIndex = 0x1211;
+uintptr_t RelativeLocation = 0x120;
+uintptr_t CurrentWeapon = 0xA68;
+uintptr_t PrimaryPickupItemEntry = 0x350;
+uintptr_t ItemDefinition = 0x18;
+uintptr_t Rarity = 0xEB;
+uintptr_t ItemName/DisplayName = 0x40;
+uintptr_t Levels = 0x178;
+uintptr_t WeaponData = 0x4F0;
+uintptr_t AmmoCount = 0xEBC;
+uintptr_t bIsTargeting = 0xE1C;
+uintptr_t ComponentVelocity = 0x168;
+uintptr_t Platform = 0x438;
+uintptr_t TimeHeatWasLastAdded = 0x1C2C;
+uintptr_t TimeOverheatedBegan = 0x1C30;
+uintptr_t OverheatValue = 0x1C24;
+uintptr_t WeaponOverheatedAnimation = 0x1C18;
+uintptr_t TargetedFortPawn = 0x1888;
+uintptr_t BoneArray = 0x5B0;
+uintptr_t BoneCache = 0x5E0;
+uintptr_t ComponentToWorld = 0x1C0;
+uintptr_t LastRenderTimeOnScreen = 0x2F0;
+uintptr_t LastSubmitTime = 0x2E8;
+uintptr_t ActorCount = 0xA8;
+}
