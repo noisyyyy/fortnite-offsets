abf3606740384739ffec9c7290e860de7a8a1be2
noisy <129625403+noisyyyy@users.noreply.github.com>
2024-05-24 13:20:11 +0200

    Update update_readme.sh

--- update_readme.sh
+++ update_readme.sh
@@ -1,7 +1,1 @@
-#!/bin/bash
 
-# Aktuellen Zeitstempel generieren
-timestamp=$(date +"%Y-%m-%d %T")
-
-# Neue Zeile mit dem Zeitstempel zur README-Datei hinzufügen
-echo "Letzte Aktualisierung: $timestamp" >> README.md
