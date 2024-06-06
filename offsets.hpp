43f5765ccd2ced757d1bb3ed3b6c6dbe9d350832
noisy <129625403+noisyyyy@users.noreply.github.com>
2024-06-06 15:07:59 +0200

    offsets.hpp aktualisieren

--- offsets.hpp
+++ offsets.hpp
@@ -4,7 +4,7 @@
 
 namespace offsets {
 
-	uintptr_t UWorld = 0x12BC1FD0 // https://discord.gg/N8Cx9B36
+	uintptr_t UWorld = 0x12BA7050 // https://discord.gg/N8Cx9B36
 	uintptr_t GNames = 0x12D85B40 // https://discord.gg/N8Cx9B36
 	uintptr_t GameState = 0x160 // https://discord.gg/N8Cx9B36
 	uintptr_t OwningGameInstance = 0x1D8 // https://discord.gg/N8Cx9B36 
@@ -35,7 +35,7 @@ namespace offsets {
 	uintptr_t WeaponOverheatedAnimation = 0x1C70 // https://discord.gg/N8Cx9B36
 	uintptr_t TargetedFortPawn = 0x18A8 // https://discord.gg/N8Cx9B36
 	uintptr_t BoneArray = 0x5B8 // https://discord.gg/N8Cx9B36 -> this is what fucked up esp
-	uintptr_t BoneCache = 0x5E0 // https://discord.gg/N8Cx9B36
+	uintptr_t BoneCache = 0x600 // https://discord.gg/N8Cx9B36
 	uintptr_t ComponentToWorld = 0x1C0 // https://discord.gg/N8Cx9B36
 	uintptr_t LastRenderTimeOnScreen = 0x2F0 // https://discord.gg/N8Cx9B36
 	uintptr_t LastSubmitTime = 0x2E8 // https://discord.gg/N8Cx9B36
